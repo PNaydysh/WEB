733791bd4bcc8f620d85422bbf04e90a664355e7
Pavel Naydysh <92798370+PNaydysh@users.noreply.github.com>
2022-05-23 23:58:11 +0300

    Update 1.js

--- zadacha12/1.js
+++ zadacha12/1.js
@@ -1,19 +1,19 @@
-let age = 10;
+let age = 9;
 let ageGroup;
 
-if(age <= 1)
+if (age <= 1)
   {
     ageGroup = "Щенки";
   }
-if(age>1 && age<=3)
+else if (age>1 && age<=3)
   {
     ageGroup = "Молодые собаки";
   }
-if(age>3 && age<=7)
+else if (age>3 && age<=7)
   {
     ageGroup = "Собаки средних лет";
   }
-if(age > 7)
+else if (age > 7)
   {
     ageGroup = "Почтенные таксоны";
   }
