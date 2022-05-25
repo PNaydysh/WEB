296475fbd0d26c6594cfb249073bb5bd4106cbf3
Pavel Naydysh <92798370+PNaydysh@users.noreply.github.com>
2022-05-25 22:41:25 +0300

    Update 21.js

--- zadacha13/21.js
+++ zadacha13/21.js
@@ -1,10 +1,10 @@
-let numbers = [1, 2, 4 ,5, 1, 4, 5];
+const numbers = [1, 2, 4 ,5, 1, 4, 5];
 let lastIndex = -1;
-let numWeSearch = 5;
+const numWeSearch = 5;
 
 for (let i = 0; i <= numbers.length - 1; i++) 
 {
- if (numbers[i] == numWeSearch)
+ if (numbers[i] === numWeSearch)
  {
    lastIndex = i;
  }
